d26901e3657644a877866dae91455a6fab1c8feb
AkashAbrrar <mabrrar9@gmail.com>
2022-05-02 17:23:38 +0600

    footer and dynamic date is added

--- src/components/Shared/Footer/Footer.js
+++ src/components/Shared/Footer/Footer.js
@@ -1,9 +1,12 @@
 import React from 'react';
 
 const Footer = () => {
+    const today = new Date();
+    const year = today.getFullYear();
     return (
-        <footer>
-            <p> <small>This is footer</small></p>
+        <footer className='bg-dark text-white text-center p-5 '>
+            <hr />
+            <p><small>Copyright Reservation &copy; {year}</small></p>
         </footer>
     );
 };
